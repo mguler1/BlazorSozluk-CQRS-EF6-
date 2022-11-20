edf7b71c8bf169e460cb7ff4f1ec49ef934b43d0
Muhammed <Muhammed@DESKTOP-RKEDQOI>
2022-11-20 16:51:47 +0300

    Repository interfaceler oluşturuldu

---
+++ Api/Core/BlazorSozluk.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using BlazorSozluk.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity, new()
+    {
+        Task<int> AddAsync(TEntity entity);
+        int Add(TEntity entity);
+        int Add(IEnumerable<TEntity> entities);
+        Task<int> AddAsync(IEnumerable<TEntity> entities);
+
+
+        Task<int> UpdateAsync(TEntity entity);
+        int Update(TEntity entity);
+
+
+        Task<int> DeleteAsync(TEntity entity);
+        int Delete(TEntity entity);
+        Task<int> DeleteAsync(Guid id);
+        int Delete(int Guid);
+        bool DeleteRange(Expression<Func<TEntity, bool>> predicate);
+        Task<bool> DeleteRangeAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> AddOrUpdateAsync(TEntity entity);
+        int AddOrUpdate(TEntity entity);
+
+        IQueryable<TEntity> AsQueryable();
+        Task<List<TEntity>> GetAll(bool noTracking = true);
+
+        Task<List<TEntity>>GetList(Expression<Func<TEntity, bool>> predicate,bool noTracking=true,Func<IQueryable<TEntity>>orderBy=null,params Expression<Func<TEntity, object>>[]includes);
+        Task<TEntity>GetByIdAsync(Guid id,bool noTracking=true,params Expression<Func<TEntity, object>>[] includes);
+        //Task<TEntity> GetSingleAsync(Expression<Func<TEntity,bool>>predicate,bool noTracking=true,params Expression<Func<>>);
+
+        Task BulkDeleteById(IEnumerable<Guid> ids);
+        Task BulkDelete(Expression<Func<TEntity,bool>>predicate);
+        Task BulkDelete(IEnumerable<TEntity> entities);
+        Task BulkUpdate(IEnumerable<TEntity> entities);
+        Task BulkAdd(IEnumerable<TEntity> entities);
+
+    }
+}
