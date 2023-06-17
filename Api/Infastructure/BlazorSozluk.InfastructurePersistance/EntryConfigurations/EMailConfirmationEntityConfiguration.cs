6bf0b1451f77c5dd59043d1f449a5ce56e2ebdea
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-06-18 00:21:03 +0300

    #1

--- Api/Infastructure/BlazorSozluk.InfastructurePersistance/EntryConfigurations/EMailConfirmationEntityConfiguration.cs
+++ Api/Infastructure/BlazorSozluk.InfastructurePersistance/EntryConfigurations/EMailConfirmationEntityConfiguration.cs
@@ -2,11 +2,7 @@
 using BlazorSozluk.InfastructurePersistance.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BlazorSozluk.InfastructurePersistance.EntryConfigurations
 {
